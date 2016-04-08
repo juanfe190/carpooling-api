904e60958bb6bf558b209e5a32f07d36b716f709
FelixPC <FelixPC>
2016-04-07 22:04:33 -0600

    heroku

--- server.js
+++ server.js
@@ -15,9 +15,11 @@ var constants = require('./App/Util/constants');
 
 //SOCKET.IO IMPLEMENTATION
 var io = require('socket.io').listen(server);
-io.set('origins', 'https://gentle-temple-29130.herokuapp.com');
-io.set('transports', ['xhr-polling']);
-io.set('polling duration', 10);
+io.configure(function () {  
+	io.set('origins', 'https://gentle-temple-29130.herokuapp.com');
+	io.set("transports", ["xhr-polling"]); 
+	io.set("polling duration", 10); 
+});
 var port = process.env.PORT || 3000;
 server.listen(port, function(){console.log('App running on port: '+port)});
 require('./socket').startConnection(io);
