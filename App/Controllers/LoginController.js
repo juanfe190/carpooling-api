c73aad2ec1c7d1c86380721741902791439a0367
FelixPC <FelixPC>
2016-05-16 20:19:42 -0600

    enviar email al verificar token

--- App/Controllers/LoginController.js
+++ App/Controllers/LoginController.js
@@ -20,7 +20,7 @@ function LoginController(request, response){
 			var token = jwt.sign({username: username}, constants.jwtPrivateKey);
 			return response.json({status: 'ok', token: token, user: objUser});
 		}else {
-			if(objUser.token != '') return response.json({status: 'denied', error: '1002', user: objUser});
+			if(objUser.token != '') return response.json({status: 'denied', error: '1002', email: objUser.email});
 			return response.json({status: 'denied', error: '1001'});
 		}
 	});
